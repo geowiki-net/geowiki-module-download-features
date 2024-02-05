c7534848d247dc2f8ad2cce1f5439aa5e8d0e974
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-02-05 10:06:33 +0100

    Restructure for Geowiki

--- src/downloadFeatures.js
+++ src/downloadFeatures.js
@@ -1,3 +1,5 @@
+import exportAll from './exportAll'
+
 module.exports = {
   id: 'download-features',
   appInit (app) {
@@ -8,9 +10,103 @@ module.exports = {
       div.appendChild(button)
 
       button.onclick = () => {
-        if (leafletGeowikiLayer.overpassFrontend) {
+        const conf = {
+          type: 'OSMXML'
         }
+
+        exportAll(leafletGeowikiLayer, conf, (err) => {
+          global.alert('done')
+        })
       }
     })
   }
 }
+
+/*
+register_hook('init', function () {
+  formExport = new form('export', formDef())
+
+  let domForm = document.createElement('form')
+  tab.content.appendChild(domForm)
+  formExport.show(domForm)
+
+  let submit = document.createElement('input')
+  submit.type = 'submit'
+  submit.value = lang('export-prepare')
+  submit.onclick = () => {
+    let progressIndicator = document.createElement('div')
+    progressIndicator.innerHTML = '<i class="fa fa-spinner fa-pulse fa-fw"></i> ' + lang('loading')
+    tab.content.appendChild(progressIndicator)
+    submit.style.display = 'none'
+
+    prepareDownload((err) => {
+      if (err) {
+        alert(err)
+      }
+
+      submit.style.display = 'block'
+      tab.content.removeChild(progressIndicator)
+      tab.unselect()
+    })
+  }
+  tab.content.appendChild(submit)
+
+  tab.on('select', () => {
+    formExport.resize()
+  })
+})
+
+ (data) => {
+  const div = document.createElement('div')
+
+  let formExport = new form('exportOne', formDef())
+
+  let domForm = document.createElement('form')
+  div.appendChild(domForm)
+  formExport.show(domForm)
+
+  let submit = document.createElement('input')
+  submit.type = 'submit'
+  submit.value = lang('export-prepare')
+  submit.onclick = () => {
+    let progressIndicator = document.createElement('div')
+    progressIndicator.innerHTML = '<i class="fa fa-spinner fa-pulse fa-fw"></i> ' + lang('loading')
+    div.appendChild(progressIndicator)
+    submit.style.display = 'none'
+
+    let conf = formExport.get_data()
+
+    conf.singleFeature = true
+
+    createDownload(conf, [ data ], (err) => {
+      if (err) {
+        alert(err)
+      }
+
+      submit.style.display = 'block'
+      div.removeChild(progressIndicator)
+    })
+  }
+  div.appendChild(submit)
+
+  global.setTimeout(() => formExport.resize(), 0)
+
+  return div
+}
+
+function formDef () {
+  let values = {}
+  Object.keys(types).forEach(type =>
+    values[type] = lang('export:' + type)
+  )
+
+  return {
+    type: {
+      name: 'Type',
+      type: 'radio',
+      values,
+      default: Object.keys(types)[0]
+    }
+  }
+}
+*/
